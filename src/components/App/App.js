03a4afef35c5ce32371077a96b705e9272de0004
Shuyang <shuyang0129@gmail.com>
2018-07-10 23:02:29 +0800

    Replace project html and css to create-react-app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,15 +5,84 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <div className="SearchBar">
+            <input placeholder="Enter A Song Title" />
+            <a>SEARCH</a>
+          </div>
+          <div className="App-playlist">
+            <div className="SearchResults">
+              <h2>Results</h2>
+              <div className="TrackList">
+                <div className="Track">
+                  <div className="Track-information">
+                    <h3>Tiny Dancer</h3>
+                    <p>Elton John | Madman Across The Water</p>
+                  </div>
+                  <a className="Track-action">+</a>
+                </div>
+                <div className="Track">
+                  <div className="Track-information">
+                    <h3>Tiny Dancer</h3>
+                    <p>Tim McGraw | Love Story</p>
+                  </div>
+                  <a className="Track-action">+</a>
+                </div>
+                <div className="Track">
+                  <div className="Track-information">
+                    <h3>Tiny Dancer</h3>
+                    <p>Rockabye Baby! | Lullaby Renditions of Elton John</p>
+                  </div>
+                  <a className="Track-action">+</a>
+                </div>
+                <div className="Track">
+                  <div className="Track-information">
+                    <h3>Tiny Dancer</h3>
+                    <p>The White Raven | Tiny Dancer</p>
+                  </div>
+                  <a className="Track-action">+</a>
+                </div>
+                <div className="Track">
+                  <div className="Track-information">
+                    <h3>Tiny Dancer - Live Album Version</h3>
+                    <p>Ben Folds | Ben Folds Live</p>
+                  </div>
+                  <a className="Track-action">+</a>
+                </div>
+              </div>
+            </div>
+            <div className="Playlist">
+              <input value='New Playlist' />
+              <div className="TrackList">
+                <div className="Track">
+                  <div className="Track-information">
+                    <h3>Stronger</h3>
+                    <p>Britney Spears | Oops!... I Did It Again</p>
+                  </div>
+                  <a className="Track-action">-</a>
+                </div>
+                <div className="Track">
+                  <div className="Track-information">
+                    <h3>So Emotional</h3>
+                    <p>Whitney Houston | Whitney</p>
+                  </div>
+                  <a className="Track-action">-</a>
+                </div>
+                <div className="Track">
+                  <div className="Track-information">
+                    <h3>It's Not Right But It's Okay</h3>
+                    <p>Whitney Houston | My Love Is Your Love</p>
+                  </div>
+                  <a className="Track-action">-</a>
+                </div>
+              </div>
+              <a className="Playlist-save">SAVE TO SPOTIFY</a>
+            </div>
+          </div>
+        </div>
+    </div>
     );
   }
 }
