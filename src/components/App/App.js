342e282b3ca0210b8611cf390bee1d49dd5841b4
Shuyang <shuyang0129@gmail.com>
2018-07-11 14:17:49 +0800

    Simulate the search function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,12 +6,24 @@ import SearchResult from '../SearchResult/SearchResult';
 import PlayList from '../PlayList/PlayList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResult: [],
+      playList: []
+    };
+  }
+
+  handleClick(searchKeyword) {
+    console.log('From App component', searchKeyword);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onClick={this.handleClick} />
           <div className="App-playlist">
             <SearchResult />
             <PlayList />
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,11 +2,33 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchKeyword: ''
+        };
+
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleSearch(e) {
+        e.preventDefault();
+        this.setState({
+            searchKeyword: e.target.value
+        });
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        this.props.onClick(this.state.searchKeyword);
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song Title" />
-                <a>SEARCH</a>
+                <input onChange={this.handleSearch} placeholder="Enter A Song Title" />
+                <a onClick={this.handleClick}>SEARCH</a>
             </div>
         );
     }
