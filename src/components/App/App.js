abb3b0983b66a9a024b869380f6f9a5a2e19c7f7
Shuyang <shuyang0129@gmail.com>
2018-07-13 11:35:07 +0800

    Avoid adding item with same id

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,19 +5,55 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResult from '../SearchResult/SearchResult';
 import PlayList from '../PlayList/PlayList';
 
-const searchResult = {
-  title: 'Tiny Dancer',
-  artist: 'Elton John',
-  album: 'Madman Across The Water'
-};
-
 const searchResultList = [
-searchResult,
-searchResult,
-searchResult,
-searchResult,
-searchResult,
-searchResult
+  {
+    id: '01',
+    title: 'Tiny Dancer',
+    artist: 'Elton John',
+    album: 'Madman Across The Water'
+  },
+  {
+    id: '02',
+    title: 'Stronger',
+    artist: 'Britney Spears',
+    album: 'Oops!... I Did It Again'
+  },
+  {
+    id: '03',
+    title: 'So Emotional',
+    artist: 'Whitney Houston',
+    album: 'Whitney'
+  },
+  {
+    id: '04',
+    title: 'It\'s Not Right But It\'s Okay',
+    artist: 'Whitney Houston',
+    album: 'My Love Is Your Love'
+  },
+  {
+    id: '05',
+    title: 'Tiny Dancer',
+    artist: 'Tim McGraw',
+    album: 'Love Story'
+  },
+  {
+    id: '06',
+    title: 'Tiny Dancer',
+    artist: 'Rockabye Baby!',
+    album: 'Lullaby Renditions of Elton John'
+  },
+  {
+    id: '07',
+    title: 'Tiny Dancer',
+    artist: 'The White Raven',
+    album: 'Tiny Dancer'
+  },
+  {
+    id: '08',
+    title: 'Tiny Dancer - Live Album Version',
+    artist: 'Ben Folds',
+    album: 'Ben Folds Live'
+  }
 ];
 
 class App extends Component {
@@ -41,10 +77,18 @@ class App extends Component {
 
   addToList(selectedItem) {
     const playListArray = this.state.playList;
-    playListArray.push(selectedItem);
-    this.setState({
-      playList: playListArray
+    let shouldUpdate = true
+    playListArray.forEach(item => {
+      if(item.id === selectedItem.id) {
+        shouldUpdate = false;
+      };
     });
+    if (shouldUpdate) {
+      playListArray.push(selectedItem);
+      this.setState({
+        playList: playListArray
+      });
+    }
   }
 
   render() {
