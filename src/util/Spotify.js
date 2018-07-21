5c90d8e3cc87e748bcc7f881e9dc7837cf1f603c
Shuyang <shuyang0129@gmail.com>
2018-07-21 17:32:47 +0800

    Adjust getAccessToken to async method

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -4,22 +4,26 @@ const client_id = '3ba5c157fd384a51913e7a1a8247c1d1'; // Your client id
 const redirect_uri = 'http://localhost:3000/'; // Your redirect uri
 
 const Spotify = {
-    getAccessToken() {
-        if(access_token) {
-            return access_token;
-        } else {
-           const url = window.location.href;
-           if(url.match(/access_token=([^&]*)/) == null) {
-                window.location = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirect_uri}`;
+    async getAccessToken() {       
+        try {
+            if(access_token) {
+                return access_token;
             } else {
-                access_token = url.match(/access_token=([^&]*)/)[1];
-                const expiresIn = parseInt(url.match(/expires_in=([^&]*)/)[1], 10);
-                setTimeout(() => {
-                    access_token = '';
-                } , expiresIn * 1000);
-                window.history.pushState('Access Token', null, '/');
+                const url = window.location.href;
+                if(url.match(/access_token=([^&]*)/) == null) {
+                    window.location = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirect_uri}`;
+                } else {
+                    access_token = url.match(/access_token=([^&]*)/)[1];
+                    const expiresIn = parseInt(url.match(/expires_in=([^&]*)/)[1], 10);
+                    setTimeout(() => {
+                        access_token = '';
+                    } , expiresIn * 1000);
+                    window.history.pushState('Access Token', null, '/');
+                }
+                return access_token;
             }
-            return access_token;
+        } catch(error) {
+            console.log(error);
         }
     },
 
